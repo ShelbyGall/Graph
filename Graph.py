fa9208a35ebf397dde69c1a2117df2ea8b22d430
Shelby Gallegos <116443555+ShelbyGall@users.noreply.github.com>
2025-03-02 20:10:20 -0800

    implemented dijkstras shortest path algorithm

--- Graph.py
+++ Graph.py
@@ -1,6 +1,8 @@
 #%%
 import Vertex as v
 from collections import deque
+import heapq
+
 
 
 class Graph:
@@ -185,7 +187,80 @@ class Graph:
                     return True
 
 
-    # TODO: Dijkstras, MST, Disjoint sets, topological sort
+    def shortest_path(self, src: v, dest: v) -> tuple:
+        # dict to keep track of the vertices that connect to each other
+        # on the shortest path from src to dest
+        v_to: dict = dict(zip(self.vertices, [None]*len(self.vertices)))
+
+        # dict to keep track of the shortest weights to each vertex
+        w_to: dict = dict(zip(self.vertices, [None]*len(self.vertices)))
+
+        w_to.update({src:0})
+        # visited set to keep track of vertices we've already visited
+        visited: set = set()
+        # use a priority queue/heap for exploring adjacent vertices
+        queue: list = []
+
+        # add the src vertex to the queue 
+        heapq.heappush(queue, (0,src))
+
+        # keep checking the queue to see if its empty
+        # if the queue is empty; searching is complete
+        # and the search was unsuccessful
+        while len(queue) != 0:
+            # grab the current vertex from the queue
+            _, curr_ver = heapq.heappop(queue)
+
+            # if the current vertex has already been visited then go to next
+            # iteration
+
+            if curr_ver in visited:
+                continue
+            
+            # if the current vertex hasnt been visited, then add it 
+            # to the visited vertex to show we are visiting it
+            visited.add(curr_ver)
+
+            # "visit" the current vertex
+            print(curr_ver)
+            
+
+            # if the current vertex is what we are searching for then 
+            # return and the search is successful
+            if curr_ver == dest:
+                print("found")
+
+                # return the list of vertices along the shortest path
+                # from src to dest and the weight it took to get there
+                path = []
+                path.append(dest)
+                c = v_to.get(dest)
+                
+                while True:
+                    path.append(c)
+                    if c == src:
+                        return (w_to.get(dest), path[::-1])
+                    c = v_to.get(c)
+                    
+
+            # if current vertex isnt what we are searching for then
+            # add all adjacent vertices of the current vertex to the 
+            # exploration queue as long as the adjacent vertex hasnt 
+            # already been visited
+            for adj_ver in self.edges.get(curr_ver):
+                if adj_ver[0] not in visited:
+                    heapq.heappush(queue, (adj_ver[1], adj_ver[0]))
+
+                    # if there is no shortest weight recorded in w_to for the adjacent vertex
+                    # or the newly discovered paths weight is smaller than the weight recorded in w_to for the adjacent vertex
+                    if (w_to.get(adj_ver[0]) == None) or (w_to.get(adj_ver[0]) > adj_ver[1] + w_to.get(curr_ver)):
+                        # update the w_to and v_to with the newly discovered path
+                        w_to.update({adj_ver[0]:adj_ver[1] + w_to.get(curr_ver)})
+                        v_to.update({adj_ver[0]: curr_ver})
+
+    # TODO: REFACTOR SHORTEST PATH SO ITS SIMPLER TO READ. MST, Disjoint sets, topological sort APPLICATION
+        
+        
 
     def __str__(self) -> str:
         ret_str: str = ""
@@ -199,7 +274,7 @@ class Graph:
         return ret_str.join(temp)
 #%%
 
-v1 = v.Vertex('1', {"branch name":"Scranton"})
+v1 = v.Vertex('1')
 v2 = v.Vertex('2')
 v3 = v.Vertex('3')
 v4 = v.Vertex('4')
@@ -210,7 +285,7 @@ v8 = v.Vertex('8')
 
 print(v1)
 print(v2)
-
+#%%
 vertices = [v1,v2,v3,v4]
 
 my_graph = Graph(vertices=vertices)
@@ -223,7 +298,6 @@ my_graph.add_edge(src=v5, dest=v2)
 
 print(my_graph)
 
-# %%
 vertices2 = [v1,v2,v3,v4,v5,v6,v7,v8]
 g2 = Graph(vertices=vertices2)
 
@@ -244,8 +318,6 @@ print()
 g2.dfs_recursive(v1,v8)
 print()
 
-
-# %%
 v999 = v.Vertex('999')
 g2.add_vertex(v999)
 
@@ -255,4 +327,15 @@ g2.dfs(v1,v999)
 print()
 g2.dfs_recursive(v1,v999)
 print()
+# %%
+
+g3 = Graph(vertices=[v1,v2,v3,v4,v5])
+g3.add_edge(src=v1, dest=v2, weight=5,directed=False)
+g3.add_edge(src=v1, dest=v3, weight=1,directed=False)
+
+g3.add_edge(src=v3, dest=v4, weight=3,directed=False)
+g3.add_edge(src=v3, dest=v5, weight=1,directed=False)
+g3.add_edge(src=v5, dest=v2, weight=2,directed=False)
+
+w, path  = g3.shortest_path(v1,v2)
 # %%
